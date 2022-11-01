b38ba61c387ee365f9244c709bf01074d0779859
istratenkonick <istratenkonick@gmail.com>
2022-11-01 11:50:17 +0200

    first commit

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\{RedirectResponse, Request, Response};
+use Illuminate\Routing\Redirector;
+use Inertia\Inertia;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Inertia\Response
+     */
+    public function index()
+    {
+        return Inertia::render('Chirps/Index', [
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Application|RedirectResponse|Redirector
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate(
+            [
+                'message' => 'required|string|max:255',
+            ]
+        );
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Chirp  $chirp
+     * @return Response
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  Chirp  $chirp
+     * @return Response
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  Chirp  $chirp
+     * @return Application|Redirector|RedirectResponse
+     * @throws AuthorizationException
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+        $this->authorize('update', $chirp);
+
+        $validated = $request->validate(
+            [
+                'message' => 'required|string|max:255',
+            ]
+        );
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Chirp  $chirp
+     * @return Application|Redirector|RedirectResponse
+     * @throws AuthorizationException
+     */
+    public function destroy(Chirp $chirp)
+    {
+        $this->authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
+    }
+}
---
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ChirpCreated;
+use App\Models\User;
+use App\Notifications\NewChirp;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendChirpCreatedNotifications implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  ChirpCreated  $event
+     * @return void
+     */
+    public function handle(ChirpCreated $event): void
+    {
+        foreach (User::query()->whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        }
+    }
+}
